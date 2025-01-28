a432e8f96e602875339cd538cfc9a89cf80fdf0b
Rocky Dewan <76462396+Rocky-Dewan@users.noreply.github.com>
2025-01-28 23:20:11 +0600

    Create Home.js

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Map from '../components/Map';
+import SearchBar from '../components/SearchBar';
+
+const Home = () => {
+  return (
+    <div>
+      <SearchBar />
+      <Map />
+    </div>
+  );
+};
+
+export default Home;
